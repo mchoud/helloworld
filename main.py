4d8ed4413c6ba29862d96c11af40d7e00f045923
mchoud <mchoud@gmail.com>
2023-09-05 16:55:28 +0700

    add variable

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 
 
 def main():
-    print("Hello, World!")
-    # test jreng
+    book = 'Dracula'
+    author = 'Bram Stoker'
+    release_year = 1897
+    goodreads_rating = 4.01
+
+    print(book)
+    print(author)
+    print(release_year)
+    print(goodreads_rating)
+
 
 if __name__ == '__main__':
     main()
 
-
+# Dracula
+# Bram
+# 1897
+# 4.01
 # Hello, World!
