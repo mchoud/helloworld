3187808dab10c3b05c80c88e22f1af3d0c91e89c
mchoud <mchoud@gmail.com>
2023-09-05 17:07:00 +0700

    setelah dibetulin di string kan jadi ok

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     release_year = 1897
     goodreads_rating = 4.01
 
-    print(book + ' is a novel by ' + author + ', published in ' + release_year + '. It has a rating of ' + goodreads_rating + ' on goodreads.')
+    print(book + ' is a novel by ' + author + ', published in ' + str(release_year) + '. It has a rating of ' + str(goodreads_rating) + ' on goodreads.')
 
 
 if __name__ == '__main__':
