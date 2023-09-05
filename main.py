587d096e6330027cb27885bcf1f2c2bba3596e5d
mchoud <mchoud@gmail.com>
2023-09-05 18:09:48 +0700

    reminder / modulo

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ def main():
     num_1 = 15
     num_2 = 12
 
-    print(f'sum of num_1 and num_2 is: {num_1 + num_2}')
-    print(f'difference of num_2 and num_1 is: {num_2 - num_1}')
-    print(f'product of num_1 and num_2 is: {num_1 * num_2}')
-    print(f'quotient of num_1 and num_2 is: {num_1 / num_2}')
-    print(f'floored quotient of num_1 and num_2 is: {num_1 // num_2}')
+    print(f'reminder of num_1 / num_2 is: {num_1 % num_2}')
 
 
 if __name__ == '__main__':
