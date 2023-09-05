2cde39d22b1aceda5c69e26ef21aa249c9586bbe
mchoud <mchoud@gmail.com>
2023-09-05 18:05:45 +0700

    # difference of num_2 and num_1 is: -3

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
 
     print(f'sum of num_1 and num_2 is: {num_1 + num_2}')
     print(f'difference of num_2 and num_1 is: {num_2 - num_1}')
+    print(f'product of num_1 and num_2 is: {num_1 * num_2}')
+    print(f'quotient of num_1 and num_2 is: {num_1 / num_2}')
+    print(f'floored quotient of num_1 and num_2 is: {num_1 // num_2}')
 
 
 if __name__ == '__main__':
